a68f218060b36dd369b8a6d16cb45626c5d35a52
manuelsavino <manuelsavino@gmail.com>
2020-12-03 13:40:28 -0500

    purge css

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,10 +2,6 @@ const colors = require('tailwindcss/colors');
 
 module.exports = {
   darkMode: 'class',
-  future: {
-    removeDeprecatedGapUtilities: true,
-    purgeLayersByDefault: true,
-  },
   purge: ['./components/**/*.js', './pages/**/*.js'],
   theme: {
     colors: {
