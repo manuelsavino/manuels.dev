e4c81cccedc9dc7c9271c4bdfa895bf505e55acb
manuelsavino <manuelsavino@gmail.com>
2020-12-04 13:34:44 -0500

    fix success emoji

--- src/pages/success.js
+++ src/pages/success.js
@@ -4,8 +4,8 @@ const Success = () => {
   return (
     <Layout>
       <div className='container mx-auto flex justify-center items-center flex-col'>
-        <h2 className='text-gray-800 text-5xl md:text-7xl font-extrabold font-sans mt-10 text-center'>
-          Success! 🥳
+        <h2 className='text-gray-800 text-5xl md:text-7xl font-extrabold font-sans mt-10 text-center flex'>
+          Success! <img src='/celebrateEmoji.png' />
         </h2>
         <h5 className='text-gray-800 text-2xl  font-sans mt-5 mb-4 text-center'>
           I will be reaching out to you shortly!
