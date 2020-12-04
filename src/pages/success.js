96bb6866d94ee46628548131184b75769bfc51ca
manuelsavino <manuelsavino@gmail.com>
2020-12-04 14:55:36 -0500

    dev initial commit

--- src/pages/success.js
+++ src/pages/success.js
@@ -4,7 +4,7 @@ const Success = () => {
   return (
     <Layout>
       <div className='container mx-auto flex justify-center items-center flex-col'>
-        <h2 className='text-gray-800 text-5xl md:text-7xl font-extrabold font-sans mt-10 text-center flex'>
+        <h2 className='text-gray-800 text-5xl md:text-7xl font-extrabold font-sans mt-10 text-center flex items-center'>
           Success! <img src='/celebrateEmoji.png' />
         </h2>
         <h5 className='text-gray-800 text-2xl  font-sans mt-5 mb-4 text-center'>
